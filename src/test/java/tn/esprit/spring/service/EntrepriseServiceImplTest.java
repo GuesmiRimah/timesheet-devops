a81384be9da30a22db2e7605a207e8a405b2c65f
Rimah GUESMI <rimah.guesmi1@esprit.tn>
2021-11-07 19:54:59 +0100

    modiff

--- src/test/java/tn/esprit/spring/service/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/EntrepriseServiceImplTest.java
@@ -47,11 +47,11 @@ public class EntrepriseServiceImplTest {
 		
 	}
 	
-	//public void testDeleteEntreprise(){
-		//es.deleteEntreprise(3);
-		//Assertions.assertNull(es.retrieveEntreprise(5));
+	public void testDeleteEntreprise(){
+		es.deleteEntreprise(2);
+		Assertions.assertNull(es.retrieveEntreprise(5));
 		
-	//}	
+	}	
 	
 	private static final Logger L = LogManager.getLogger(EntrepriseServiceImpl.class);
 	
@@ -62,7 +62,7 @@ public class EntrepriseServiceImplTest {
 			testRetrieveAllEntreprises();
 			testAddEntreprise();
 			testUpdateEntreprise();
-			//testDeleteEntreprise();
+			testDeleteEntreprise();
 			L.info("Out of testAll()");
 		}catch(Exception e){
 			L.error("Out of testAll()with error:" +e);
